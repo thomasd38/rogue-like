2bca00fd8f56bdde8bb0f9489c0c873b6faa3930
Thomas Darrigol <thomas.darrigol@free.fr>
2026-04-25 18:51:57 +0200

    fix app creation

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cannon-roguelike-v1';
+const CACHE_NAME = 'cannon-roguelike-v2';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
@@ -11,11 +11,12 @@ const ASSETS_TO_CACHE = [
   './js/projectile.js',
   './js/upgrades.js',
   './js/input.js',
-  './img/icon-512.png'
+  './img/icon-512.png',
+  './manifest.json'
 ];
 
-// Install event - caching assets
 self.addEventListener('install', (event) => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME).then((cache) => {
       return cache.addAll(ASSETS_TO_CACHE);
@@ -23,26 +24,31 @@ self.addEventListener('install', (event) => {
   );
 });
 
-// Activate event - cleaning up old caches
 self.addEventListener('activate', (event) => {
   event.waitUntil(
-    caches.keys().then((cacheNames) => {
-      return Promise.all(
-        cacheNames.map((cacheName) => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
+    Promise.all([
+      self.clients.claim(),
+      caches.keys().then((cacheNames) => {
+        return Promise.all(
+          cacheNames.map((cacheName) => {
+            if (cacheName !== CACHE_NAME) {
+              return caches.delete(cacheName);
+            }
+          })
+        );
+      })
+    ])
   );
 });
 
-// Fetch event - serving from cache or network
 self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
+      return response || fetch(event.request).catch(() => {
+        if (event.request.mode === 'navigate') {
+          return caches.match('./index.html');
+        }
+      });
     })
   );
 });
