953a4bb4dbcf29d5429ec90d46af3a81ce656ed1
Thomas Darrigol <thomas.darrigol@free.fr>
2026-04-25 22:00:58 +0200

    change name

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# 🎮 Project: Cannon Roguelike (working title)
+# 🎮 Project: Void Drifter
 
 ## 🧠 Project Overview
 
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'cannon-roguelike-v2';
+const CACHE_NAME = 'void-drifter-v1';
 const ASSETS_TO_CACHE = [
   './',
   './index.html',
